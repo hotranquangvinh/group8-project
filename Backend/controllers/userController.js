773f08aa057775a77cdf889c40963908498f0035
nnguyenngoctann-glitch <nnguyenngoctann@gmail.com>
2025-10-16 10:40:08 +0700

    Cập nhật lại API PUT/DELETE cho User

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,18 +1,32 @@
-let users = []; // mảng tạm nếu chưa dùng MongoDB
+let users = []; // mảng tạm
+
+// POST: tạo user mới
+exports.createUser = (req, res) => {
+  const user = req.body;
+  users.push(user);
+  res.status(201).json(user);
+};
+
+// GET: lấy danh sách user
+exports.getUsers = (req, res) => {
+  res.json(users);
+};
+
 // PUT: sửa user
 exports.updateUser = (req, res) => {
- const { id } = req.params;
- const index = users.findIndex(u => u.id == id);
- if (index !== -1) {
- users[index] = { ...users[index], ...req.body };
- res.json(users[index]);
- } else {
- res.status(404).json({ message: "User not found" });
- }
+  const { id } = req.params;
+  const index = users.findIndex(u => u.id == id);
+  if (index !== -1) {
+    users[index] = { ...users[index], ...req.body };
+    res.json(users[index]);
+  } else {
+    res.status(404).json({ message: "User not found" });
+  }
 };
+
 // DELETE: xóa user
 exports.deleteUser = (req, res) => {
- const { id } = req.params;
- users = users.filter(u => u.id != id);
- res.json({ message: "User deleted" });
+  const { id } = req.params;
+  users = users.filter(u => u.id != id);
+  res.json({ message: "User deleted" });
 };
