37160e29739376bcaa8cfa5821d5f7b2edbbf577
nnguyenngoctann-glitch <nnguyenngoctann@gmail.com>
2025-10-23 08:24:05 +0700

    Cập nhật backend

--- Backend/server.js
+++ Backend/server.js
@@ -8,5 +8,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api/auth', require('./routes/authRoutes'));
+app.use('/api/users', require('./routes/user'));
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
